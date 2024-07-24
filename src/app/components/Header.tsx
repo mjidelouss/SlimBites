fd9d6ce7ad1c3f2dc774a45408fbb80774e0f54c
mjidelouss <mjid.elouss@gmail.com>
2024-07-24 11:40:31 +0200

    Fixed a Header Error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,11 +3,8 @@ import { angkor } from "./fonts";
 const Header = () => {
     return (
       <div className="text-center mb-4">
-        <p className={` text-xl text-gray-700 dark:text-gray-300 ${angkor.className} `}>
-        Calculate Your <span className="text-blue-400">Daily Intake</span> to Achieve
-        </p>
-        <p className={` text-xl text-gray-700 dark:text-gray-300 ${angkor.className} `}>
-        Your Desired <span className="text-red-400"> Weight Loss</span>
+        <p className={`  text-gray-700 dark:text-gray-300 ${angkor.className} `}>
+        Calculate Your <span className="text-blue-400">Daily Intake</span> to Achieve Your Desired <span className="text-red-400"> Weight Loss</span>
         </p>
       </div>
     );

974d3228c3229b7c8c4ccc7efc244f21340a41fc
mjidelouss <mjid.elouss@gmail.com>
2024-07-24 11:42:18 +0200

    Changed a p tag to h3 tag

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,9 +3,9 @@ import { angkor } from "./fonts";
 const Header = () => {
     return (
       <div className="text-center mb-4">
-        <p className={`  text-gray-700 dark:text-gray-300 ${angkor.className} `}>
+        <h3 className={`  text-gray-700 dark:text-gray-300 ${angkor.className} `}>
         Calculate Your <span className="text-blue-400">Daily Intake</span> to Achieve Your Desired <span className="text-red-400"> Weight Loss</span>
-        </p>
+        </h3>
       </div>
     );
   };
